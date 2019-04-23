c61d774e910db3a0ac722250086323278039a6f7
MC <mcmailbox014@gmail.com>
2019-04-22 23:52:38 -0400

    Added stars

---
+++ LichLagoon/Assets/followPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPoint : MonoBehaviour
+{
+
+    public GameObject targetObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = targetObj.transform.position;
+    }
+}

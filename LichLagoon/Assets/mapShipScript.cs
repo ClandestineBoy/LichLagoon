0fe5fbd3b08639dd19badbec2ca26c18a8e6ce57
MC <mcmailbox014@gmail.com>
2019-05-07 01:01:05 -0400

    Big boat bigger intro

---
+++ LichLagoon/Assets/mapShipScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapShipScript : MonoBehaviour
+{
+    public Vector3 startPos, target;
+    public GameObject targetObj;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = this.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = targetObj.transform.localPosition;
+        this.transform.localPosition = Vector3.MoveTowards(this.transform.localPosition, target, Time.deltaTime * speed);
+    }
+}

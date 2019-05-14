7927ef4b72fbb9cd7a29a23d4879c45b0788742c
Luke Brockmann <lb3297@nyu.edu>
2019-05-14 18:28:16 -0400

    Camera broke

---
+++ LichLagoon/Assets/Scripts/cameraClamp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraClamp : MonoBehaviour
+{
+
+    private float rotY;
+    public float rotMin;
+
+    public float rotMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotY = this.transform.eulerAngles.y;
+        rotY = Mathf.Clamp(rotY, rotMin, rotMax);
+        this.transform.eulerAngles = new Vector3(this.transform.eulerAngles.x, rotY, this.transform.eulerAngles.z);
+
+    }
+}
